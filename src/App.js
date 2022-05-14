e5a319cd58c5585d77d2d55669e3c8af17d8f868
Matheus Felipe <mf.mf98.23@gmail.com>
2022-05-13 21:34:32 -0300

    Implementação de counter simples com react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+const COUNTDOWN_INITIAL_TIME_IN_SECODS = 25 * 60 //25 minutes
 
 function App() {
+  const [secondsAmount, setSecondsAmount] = useState(COUNTDOWN_INITIAL_TIME_IN_SECODS)
+  
+  useEffect(()=>{
+    if(secondsAmount > 0) {
+      setTimeout(() => {
+        setSecondsAmount(state => state - 1);
+      }, 1000)
+    } else {
+      alert('Chegou');
+    }
+  }, [secondsAmount])
+
+  const minutes = Math.floor(secondsAmount / 60);
+  const seconds = secondsAmount % 60;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <span>{String(minutes).padStart(2, '0')}</span>
+        <span>:</span>
+        <span>{String(seconds).padStart(2, '0')}</span>
+      </div>
   );
 }
 
